b0e50a129bf944393a9c2df09e78c3d88afb35ac
Purushottam <purushottamn8@gmail.com>
2018-01-15 22:04:04 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,26 +1,27 @@
-
-
-
+//reverse the String having only lowerr alphabates
 public class ReverseString {
 
 	GettingArrary gda=new  GettingArrary();
 	void reversingstr() {
-		int count =0;
- 	
-		for(int i=0;i<gda.str.length();i++)
+		int count =0,i=0;
+ 	//char ary[] =new char[gda.str.length()];
+		while(i<gda.str.length())
 		{
-			count=0;
 			if((gda.ary[i]>='a'&&gda.ary[i]<='z')) {
-			count+=1;
+			i++;
+			count=i;
 			}
-		else System.out.println("not a valid string");
-		break;
+		else {
+			System.out.println("not a valid string");
+			break;
 		}
-		if(count>1) {
-			for(int i=gda.n-1;i>=0;i--) {
+		
+		}
+		if(count==gda.str.length()) {
+			for( i=gda.n-1;i>=0;i--) {
 			System.out.print(gda.ary[i]);
 		}
 			
 	}
-}
+		}
 }
