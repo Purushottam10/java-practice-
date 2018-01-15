ac1c61c4e6a3ba9c5ed665bdc7bd6aa48477544c
Purushottam <purushottamn8@gmail.com>
2018-01-15 11:46:02 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,23 +1,26 @@
 
+
+
 public class ReverseString {
 
 	GettingArrary gda=new  GettingArrary();
 	void reversingstr() {
 		int count =0;
- 	//char ary[] =new char[gda.str.length()];
+ 	
 		for(int i=0;i<gda.str.length();i++)
 		{
+			count=0;
 			if((gda.ary[i]>='a'&&gda.ary[i]<='z')) {
-			//ary[i]=gda.ary[i];
 			count+=1;
 			}
 		else System.out.println("not a valid string");
 		break;
 		}
-		if(count>0) {
+		if(count==gda.str.length()) {
 			for(int i=gda.n-1;i>=0;i--) {
 			System.out.print(gda.ary[i]);
 		}
-	}
+			
+	}else System.out.println("not a valid string");
 }
 }
