9c83d29b16e8b8eb2ee92fa9bf5f327ae8dae273
Purushottam <purushottamn8@gmail.com>
2018-01-18 23:38:04 +0530

    Update PalidromeString.java

--- PalidromeString.java
+++ PalidromeString.java
@@ -3,22 +3,21 @@ public class PalidromeString {
 	GettingArrary gd= new GettingArrary();
 	boolean revesingString() {
 		//System.out.println("hello");
-		boolean flag=false;
+		boolean flag=true;
 		int i=0;
 		while(i<(gd.str.length())/2+1) {
-			flag=false;
-			if(gd.ary[i]==gd.ary[gd.str.length()-i-1]) {
-			     flag=true;
+				if(gd.ary[i]!=gd.ary[gd.str.length()-i-1]) {
+			     flag=false;
 		      }
 			else {
-				flag=false;
+				flag=true;
 			}
 			i++;
 		}
 		if(flag==true) {
-			 System.out.println("String is Armstrong:"+gd.str);
+			 System.out.println("String is Armstrong ::"+gd.str);
 		}
-		else System.out.println("String is not Armstron "+gd.str);
+		else System.out.println("String is not Armstron ::"+gd.str);
 		return true;
 	}
 
