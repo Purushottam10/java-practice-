7e21f0aa4733c016441ff6065e5d29fd8a8c95cb
Purushottam <purushottamn8@gmail.com>
2018-01-18 22:26:56 +0530

    Add files via upload

---
+++ ImpPalindromString.java
@@ -0,0 +1,10 @@
+
+public class ImpPalindromString {
+	public static void main(String[] args) {
+		GettingArrary gta=new GettingArrary();
+		gta.getdata();
+		PalidromeString arst=new PalidromeString();
+		arst.gd=gta;
+		arst.revesingString();
+	}
+}
---
+++ PalidromeString.java
@@ -0,0 +1,25 @@
+
+public class PalidromeString {
+	GettingArrary gd= new GettingArrary();
+	boolean revesingString() {
+		//System.out.println("hello");
+		boolean flag=false;
+		int i=0;
+		while(i<(gd.str.length())/2+1) {
+			flag=false;
+			if(gd.ary[i]==gd.ary[gd.str.length()-i-1]) {
+			     flag=true;
+		      }
+			else {
+				flag=false;
+			}
+			i++;
+		}
+		if(flag==true) {
+			 System.out.println("String is Armstrong:"+gd.str);
+		}
+		else System.out.println("String is not Armstron "+gd.str);
+		return true;
+	}
+
+}
