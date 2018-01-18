8bdd3ed1e4879a8f7005644a2c49fbbe1805b5be
Purushottam <purushottamn8@gmail.com>
2018-01-18 22:03:42 +0530

    Add files via upload

---
+++ ImpArmstrongString.java
@@ -0,0 +1,10 @@
+
+public class ImpArmstrongString {
+	public static void main(String[] args) {
+		GettingArrary gta=new GettingArrary();
+		gta.getdata();
+		ArmstrongString arst=new ArmstrongString();
+		arst.gd=gta;
+		arst.revesingString();
+	}
+}
